c447deea3cc1b9abd335d67d76e69d83e45d3aab
Sergey Kononenko <hackfeed@yandex.ru>
2021-02-22 16:54:04 +0300

    Add basic setup and starting apps

---
+++ 02_basics/01_instances/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+  data() {
+    return {
+      courseGoalA: "Finish the course and learn Vue!",
+      courseGoalB: "Master Vue and build amazing apps!",
+      vueLink: "https://vuejs.org",
+    };
+  },
+  methods: {
+    outputGoal() {
+      const randomNumber = Math.random();
+      if (randomNumber < 0.5) {
+        return this.courseGoalA;
+      }
+      return this.courseGoalB;
+    },
+  },
+});
+
+app.mount("#user-goal");
