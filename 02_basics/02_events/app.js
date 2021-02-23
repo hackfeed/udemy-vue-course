5b5ddd17b7daf311821144fa0ceea1de49398b3f
Sergey Kononenko <hackfeed@yandex.ru>
2021-02-23 13:50:10 +0300

    Add event bindings and data bindings

--- 02_basics/02_events/app.js
+++ 02_basics/02_events/app.js
@@ -3,19 +3,40 @@ const app = Vue.createApp({
     return {
       counter: 0,
       name: "",
+      lastName: "",
     };
+  },
+  watch: {
+    // name(value) {
+    //   this.fullname = `${value} Kononenko`;
+    // },
+    counter(value) {
+      if (value < 0) {
+        this.counter = 0;
+      }
+    },
+  },
+  computed: {
+    fullname() {
+      console.log("Running again...");
+      if (this.name === "" || this.lastName === "") {
+        return "";
+      }
+      return `${this.name} ${this.lastName}`;
+    },
   },
   methods: {
     add() {
       this.counter++;
     },
     reduce() {
-      if (this.counter) {
-        this.counter--;
-      }
+      this.counter--;
     },
     setName(event) {
       this.name = event.target.value;
+    },
+    resetInput() {
+      this.name = "";
     },
     submitForm() {
       alert("Submitted!");
