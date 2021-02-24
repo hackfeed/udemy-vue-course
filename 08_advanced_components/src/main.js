a15714313b1f28b012435be78fca9e81bd9721e4
Sergey Kononenko <hackfeed@yandex.ru>
2021-02-24 20:55:43 +0300

    Add inject and provide examples

---
+++ 08_advanced_components/src/main.js
@@ -0,0 +1,15 @@
+import ActiveElement from './components/ActiveElement.vue';
+import App from './App.vue';
+import KnowledgeBase from './components/KnowledgeBase.vue';
+import KnowledgeElement from './components/KnowledgeElement.vue';
+import KnowledgeGrid from './components/KnowledgeGrid.vue';
+import { createApp } from 'vue';
+
+const app = createApp(App);
+
+app.component('active-element', ActiveElement);
+app.component('knowledge-base', KnowledgeBase);
+app.component('knowledge-element', KnowledgeElement);
+app.component('knowledge-grid', KnowledgeGrid);
+
+app.mount('#app');
