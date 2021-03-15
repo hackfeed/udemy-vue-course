3ed44aaa4e861a3026e0741ce4eb7aafae81494e
Sergey Kononenko <hackfeed@yandex.ru>
2021-03-16 01:01:30 +0300

    Add simple project with Vuex + vue-router

---
+++ proj_vueshop/src/main.js
@@ -0,0 +1,14 @@
+import App from "./App.vue";
+import BaseBadge from "./components/ui/BaseBadge.vue";
+import { createApp } from "vue";
+import router from "./router.js";
+import store from "./store/index.js";
+
+const app = createApp(App);
+
+app.use(router);
+app.use(store);
+
+app.component("base-badge", BaseBadge);
+
+app.mount("#app");
---
+++ proj_vueshop/src/store/index.js
@@ -0,0 +1,38 @@
+import cartModule from "./modules/cart.js";
+import { createStore } from "vuex";
+import productsModule from "./modules/products.js";
+
+const store = createStore({
+  modules: {
+    prods: productsModule,
+    cart: cartModule,
+  },
+  state() {
+    return {
+      isLoggedIn: false,
+    };
+  },
+  mutations: {
+    login(state) {
+      state.isLoggedIn = true;
+    },
+    logout(state) {
+      state.isLoggedIn = false;
+    },
+  },
+  actions: {
+    login(context) {
+      context.commit("login");
+    },
+    logout(context) {
+      context.commit("logout");
+    },
+  },
+  getters: {
+    isAuthenticated(state) {
+      return state.isLoggedIn;
+    },
+  },
+});
+
+export default store;
