7cbbb50f6b317cf23c2b453fff0f8a119caef461
Sergey Kononenko <hackfeed@yandex.ru>
2021-03-28 23:56:29 +0300

    Add project base

---
+++ proj_find_coach/src/router.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import CoachDetail from "./pages/coaches/CoachDetail.vue";
+import CoachRegistration from "./pages/coaches/CoachRegistration.vue";
+import CoachesList from "./pages/coaches/CoachesList.vue";
+import ContactCoach from "./pages/requests/ContactCoach.vue";
+import NotFound from "./pages/NotFound.vue";
+import RequestsReceived from "./pages/requests/RequestsReceived.vue";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { path: "/", redirect: "/coaches" },
+    { path: "/coaches", component: CoachesList },
+    {
+      path: "/coaches/:id",
+      component: CoachDetail,
+      children: [{ path: "contact", component: ContactCoach }],
+    },
+    { path: "/register", component: CoachRegistration },
+    { path: "/requests", component: RequestsReceived },
+    { path: "/:notFound(.*)", component: NotFound },
+  ],
+});
+
+export default router;
